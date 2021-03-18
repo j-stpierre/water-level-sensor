fd8c91fb1ec9b718c7e9d8fc7022b836cb9ed501
James St-Pierre <jamesamstpierre@gmail.com>
2021-03-18 17:03:38 -0400

    Add config file for .env

--- tests/unit/test_mqtt/test_mqtt.py
+++ tests/unit/test_mqtt/test_mqtt.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import json
+import config
 from sensor.mqtt import mqtt
 from unittest import mock, TestCase
 from paho.mqtt import client as mqtt_client
@@ -8,10 +9,10 @@ from paho.mqtt import client as mqtt_client
 class Test_mqtt(TestCase):
     
     def setUp(self):
-        self.username = os.environ['USERNAME']
-        self.password = os.environ['PASSWORD']
-        self.broker = os.environ['BROKER']
-        self.port = 1883
+        self.username = config.config['USERNAME']
+        self.password = config.config['PASSWORD']
+        self.broker = config.config['BROKER']
+        self.port = config.config['PORT']
         self.client = mqtt.Mqtt(self.username, self.password, self.broker, self.port)
         
     def test_mqtt_constructor(self):
