fcef21cf6805161dd79ccfe32f0131bb38e413a5
James St-Pierre <jamesamstpierre@gmail.com>
2021-03-18 23:04:19 -0400

    Add sensor main

---
+++ sensor.py
@@ -0,0 +1,25 @@
+import json
+import config
+from sensor.mqtt import mqtt
+from sensor.capture import capture
+
+def main():
+    broker = mqtt.Mqtt(
+                    config.config['USERNAME'],
+                    config.config['PASSWORD'],
+                    config.config['BROKER'],
+                    config.config['PORT'])
+
+    try:
+        broker.connect()
+        sensor = capture.Capture(6,19)
+        sensor.setStartState()
+        ditance = sensor.getAverageDistance()
+        broker.publish(config.config['TOPIC'], distance)
+
+    except:
+        print("Measurement stopped")
+        sensor.cleanup()
+
+if __name__ == '__main__':
+    main()
